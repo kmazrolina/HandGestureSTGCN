2301af0ca1e6ae5c679dc17cca5760de7278a710
Karolina Źróbek <121491288+kmazrolina@users.noreply.github.com>
2025-01-19 18:41:48 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,45 +8,45 @@
 #SBATCH --gres=gpu
 
 ## Setup Virtual Env
-#python -m venv venv
+python -m venv venv
 source ../venv/bin/activate
 
 
-# ##Install dependencies
-# pip install \
-# gdown \
-# pandas \
-# numpy \
-# torch \
-# torch_geometric \
-# networkx \
-# mediapipe \
-# opencv-python \
-# scikit-learn \
-# matplotlib \
-# jupyter
+##Install dependencies
+pip install \
+gdown \
+pandas \
+numpy \
+torch \
+torch_geometric \
+networkx \
+mediapipe \
+opencv-python \
+scikit-learn \
+matplotlib \
+jupyter
 
 
-# ## Download Data
-# mkdir data
-# cd data
+## Download Data
+mkdir data
+cd data
 
-# #download video frames
-# gdown --no-check-certificate --folder https://drive.google.com/drive/folders/1GJ3ZzidMngAK5lbcLtX5aENfXelIFAQe
-# mv frames/frames01.tgz frames/frames02.tgz frames/frames03.tgz frames/frames04.tgz frames/frames05.tgz .
-# for file in *.tgz; do
-#   tar -xvzf "$file"
-# done
-# rm frames01.tgz frames02.tgz frames03.tgz frames04.tgz frames05.tgz
+#download video frames
+gdown --no-check-certificate --folder https://drive.google.com/drive/folders/1GJ3ZzidMngAK5lbcLtX5aENfXelIFAQe
+mv frames/frames01.tgz frames/frames02.tgz frames/frames03.tgz frames/frames04.tgz frames/frames05.tgz .
+for file in *.tgz; do
+  tar -xvzf "$file"
+done
+rm frames01.tgz frames02.tgz frames03.tgz frames04.tgz frames05.tgz
 
-# #download videos for inference only
-# gdown --no-check-certificate https://drive.google.com/drive/folders/1GJ3ZzidMngAK5lbcLtX5aENfXelIFAQe
-# tar -xvzf videos01.tgz
+#download videos for inference only
+gdown --no-check-certificate https://drive.google.com/drive/folders/1GJ3ZzidMngAK5lbcLtX5aENfXelIFAQe
+tar -xvzf videos01.tgz
 
 
-# #download annotations
-# gdown --no-check-certificate --folder https://drive.google.com/drive/folders/1-mihJEIFoNDpfo1puF8xAMJz6PGVKsBD
+#download annotations
+gdown --no-check-certificate --folder https://drive.google.com/drive/folders/1-mihJEIFoNDpfo1puF8xAMJz6PGVKsBD
 
-# #extract hand keypoints from video frames
-# cd ..
+#extract hand keypoints from video frames
+cd ..
 python keypoint_detection.py 
